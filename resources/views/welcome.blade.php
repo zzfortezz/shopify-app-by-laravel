4c68a35ae35904d63894c5d86d4f69b32a6b15ef
Quang Đạt <datprpo.net@gmail.com>
2018-11-07 14:58:11 +0700

    build conatiner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 @extends('shopify-app::layouts.default')
 @section('styles')
+    <link rel="stylesheet" href="https://sdks.shopifycdn.com/polaris/2.12.1/polaris.min.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -10,6 +11,25 @@
     <div class="container-fluid">
         <div class="section-header">
             <a class="btn btn-primary">Add Size</a>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="section-summary">
+                    <h2>Select Currencies</h2>
+                    <p>Select the currencies that will appear in the Currency Switcher on your website.</p>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="section-content">
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
