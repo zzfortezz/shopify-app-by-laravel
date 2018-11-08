9c8c4f2cf93ae17786bd27924fd916f19e7e89b9
Quang Đạt <datprpo.net@gmail.com>
2018-11-08 13:58:22 +0700

    add font Monteserrat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 @extends('shopify-app::layouts.default')
 @section('styles')
     @parent
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,500,700&amp;subset=vietnamese" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.scss">
 @endsection
