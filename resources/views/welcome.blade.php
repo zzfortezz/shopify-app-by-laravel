fdb79eceba1bb920d6ef2f296bc27af35d729b46
Quang Đạt <datprpo.net@gmail.com>
2018-11-08 14:14:02 +0700

    add Editable

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 @section('styles')
     @parent
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,500,700&amp;subset=vietnamese" rel="stylesheet">
+    <link rel="stylesheet" href="libs/edite-table/jquery.edittable.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.scss">
 @endsection
@@ -26,8 +27,14 @@
                             <input type="text" class="form-control" id="title-table" placeholder="ex: T-shirt size">
                         </div>
                         <div class="form-group">
-                            <label for="tab-description">Description</label>
-                            <textarea class="form-control" id="tab-description" rows="4"></textarea>
+                            <label for="table-size-description">Description</label>
+                            <textarea class="form-control" id="table-size-description" rows="4"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="table-size">Description</label>
+                            <textarea class="form-control" id="table-size" rows="4">
+                                [["Size","Bust","Waist","Hips"],["8","32","25","35"],["10","34","27","37"],["12","36","29","39"]]
+                            </textarea>
                         </div>
                     </form>
                 </div>
@@ -39,6 +46,7 @@
 @section('scripts')
     @parent
     <script src="libs/tinymce/js/tinymce/tinymce.min.js"></script>
+    <script src="libs/edite-table/jquery.edittable.min.js"></script>
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -50,7 +58,20 @@
                 title: 'Welcome'
             })
             tinymce.init({
-                selector: '#tab-description'
+                selector: '#table-size-description',
+                branding: false // To disable "Powered by TinyMCE" branding: false // To disable "Powered by TinyMCE"
+            });
+
+            var mytable = $('#table-size').editTable({
+                data: [['']],           // Fill the table with a js array (this is overridden by the textarea content if not empty)
+                tableClass: 'inputtable',   // Table class, for styling
+                jsonData: false,        // Fill the table with json data (this will override data property)
+                headerCols: false,      // Fix columns number and names (array of column names)
+                maxRows: 99,           // Max number of rows which can be added
+                first_row: true,        // First row should be highlighted?
+                row_template: false,    // An array of column types set in field_templates
+                field_templates: false, // An array of custom field type objects
+                
             });
 
         });
