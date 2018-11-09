8b76ca0f890ce4633138125cfb1e94ec1a5bf94c
Quang Đạt <datprpo.net@gmail.com>
2018-11-09 18:02:12 +0700

    add, controler, model and update migration

---
+++ app/Http/Controllers/SizeGuideController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SizeGuide;
+use Illuminate\Http\Request;
+
+class SizeGuideController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SizeGuide  $sizeGuide
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SizeGuide $sizeGuide)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SizeGuide  $sizeGuide
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SizeGuide $sizeGuide)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SizeGuide  $sizeGuide
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SizeGuide $sizeGuide)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SizeGuide  $sizeGuide
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SizeGuide $sizeGuide)
+    {
+        //
+    }
+}
