b22bcd6f4fbe5a0911e9735fb76f3c71645bf5e1
Quang Dat <datrp.net@gmail.com>
2018-11-12 01:35:57 +0700

    update link asset

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,9 +2,9 @@
 @section('styles')
 
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,500,700&amp;subset=vietnamese" rel="stylesheet">
-    <link rel="stylesheet" href="libs/edite-table/jquery.edittable.min.css">
+    <link rel="stylesheet" href="{{asset('libs/edite-table/jquery.edittable.min.css')}}" type="text/css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.scss">
+    <link rel="stylesheet" href="{{ asset('css/style.scss') }}" type="text/css">
 @endsection
 
 
