2a12254657e7870d377fd13b1dd18e0d3f5a650e
Quang Đạt <datprpo.net@gmail.com>
2018-11-11 16:17:02 +0700

    test route

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Application Service Providers...
          */
+        \OhMyBrew\ShopifyApp\ShopifyAppProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
