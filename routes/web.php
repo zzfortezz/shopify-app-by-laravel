4ca608482be96fdbb823c0c2dfabf31ea629f818
Quang Đạt <datprpo.net@gmail.com>
2018-11-11 16:53:36 +0700

    test route

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware(['auth.shop'])->name('home');
 
-Route::group(['prefix' => 'admin'], function(){
+Route::group(['prefix' => 'admin', 'middleware' => 'auth.shop'], function(){
     //Create route with name is data.crete, and uses controler SizeGuideController and call to function store
     Route::post('/data/store', ['uses' => 'SizeGuideController@store ', 'as' => 'data.store']);
-})->middleware(['auth.shop']);
+});
 //Route::post('/data/store', ['uses' => 'SizeGuideController@store ', 'as' => 'data.create']);
 
 //Route::post('/data/store', function () {
