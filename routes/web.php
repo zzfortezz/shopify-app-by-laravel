c657eb0d013c3f4f91a29a06e8f3e501642ec42d
Quang Đạt <datprpo.net@gmail.com>
2018-11-11 17:13:31 +0700

    fix syntax erroe

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth.shop'], function(){
     //Create route with name is data.crete, and uses controler SizeGuideController and call to function store
-    Route::post('/data/store', ['uses' => 'SizeGuideController@store ', 'as' => 'data.store']);
+    Route::post('/data/store', ['uses' => 'SizeGuideController@store', 'as' => 'data.store']);
 });
 //Route::post('/data/store', ['uses' => 'SizeGuideController@store ', 'as' => 'data.create']);
 
