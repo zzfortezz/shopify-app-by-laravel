fec54f1dc0351e82c44d0b827a5e328502d2ef39
Quang Đạt <datprpo.net@gmail.com>
2018-11-12 17:15:56 +0700

    fix view

--- app/Http/Controllers/SizeGuideController.php
+++ app/Http/Controllers/SizeGuideController.php
@@ -29,7 +29,7 @@ class SizeGuideController extends Controller
      */
     public function create()
     {
-        return view('home')->with('sizeguieds', [] );
+        return view('edit')->with('sizeguieds', [] );
     }
 
     /**
