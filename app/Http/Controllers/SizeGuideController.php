f9eeedf6f19e733fb5ce6b4fe87251c04fb6b4c0
Quang Đạt <datprpo.net@gmail.com>
2018-11-13 01:47:58 +0700

    add header to response

--- app/Http/Controllers/SizeGuideController.php
+++ app/Http/Controllers/SizeGuideController.php
@@ -117,7 +117,17 @@ class SizeGuideController extends Controller
         $shop_id = $shops->id;
 
         $sizeguide = $sizeGuide::query()->where('shop_id', "$shop_id")->first();
-
-        return response()->json($sizeguide, 200);
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        header("Access-Control-Allow-Credentials: true");
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Content-Type, *");
+//        echo json_encode($sizeguide);
+        return response()
+            ->withHeaders([
+        'Access-Control-Allow-Methods' => 'GET',
+        'Access-Control-Allow-Credentials' => true,
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Headers' => 'Content-Type, *'
+            ])->json($sizeguide);
     }
 }
