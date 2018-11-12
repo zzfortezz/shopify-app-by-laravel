40c0bf153910d068a2428d07ab18afcf41c2677c
Quang Đạt <datprpo.net@gmail.com>
2018-11-12 11:54:53 +0700

    test link asset

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
 
+{{ var_dump($sizeguides) }}
 @section('content')
     <a href="/shopify-app-by-laravel/edit">click</a>
     <p>You are: {{ ShopifyApp::shop()->shopify_domain }}</p>
