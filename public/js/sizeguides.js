9716927c10a7f620ea5bb4edc4deb0005f2ba08e
Quang Đạt <datprpo.net@gmail.com>
2018-11-13 02:09:19 +0700

    comment js

--- public/js/sizeguides.js
+++ public/js/sizeguides.js
@@ -1,8 +1,8 @@
 $(function(){
     console.log('init size guide');
-    var el_sizeguide = document.createElement("div");
+   /* var el_sizeguide = document.createElement("div");
     el_sizeguide.className = "sizeguide-container";
 
     var h = document.getElementById("myH2");
-    h.insertAdjacentHTML("afterend", $('#ProductPrice'));
+    h.insertAdjacentHTML("afterend", $('#ProductPrice'));*/
 });
