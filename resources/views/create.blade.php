84e7a1956501e31e5348096051d0423925568b79
Quang Đạt <datprpo.net@gmail.com>
2018-11-14 14:39:57 +0700

    update link

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -167,7 +167,7 @@
                     data:{
                         condition: value
                     },
-                    url: '/get/condition',
+                    url: 'public/get/condition',
                     success: function (resp){
                         console.log(resp)
                     },
