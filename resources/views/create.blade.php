0796a558635e08fd58e57790829f675172ae5cfe
Quang Đạt <datprpo.net@gmail.com>
2018-11-14 16:35:36 +0700

    test console

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -173,10 +173,13 @@
                         if( resp.success ){
                             var html_option = '';
                             resp.data.each(function (idx, value){
+                                const{id, title} = value;
                                 html_option +="<option value='" + value.id + "'>" + value.title + "</option>";
+                                console.log('<option value="${id}">${title}<option>');
                             });
                             $('.value-condition').html(html_option);
-                            console.log({resp,html_option});
+                            console.log({html_option});
+                            console.table([resp.data]);
                         }
                     },
                     error: function (resp) {
